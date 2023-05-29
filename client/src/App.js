52f02df414f2730ca6272aa11213d040aefd539a
janibehm <jani.behm@gmail.com>
2023-05-29 11:35:38 +0300

    merged jt to dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import EventForm from './components/EventForm'
-import Navigation from './components/Navigation';
-import Home from './components/Home';
-
-function App() {
-  return (
-    <Router>
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/EventForm" element={<EventForm />} />
-    </Routes>
-  </Router>
-=======
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import EventForm from "./components/EventForm";
 import Layout from "./components/Layout";
+/* import Home from "./components/Home"; */
 
 function App() {
   return (
@@ -25,13 +9,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            
+        {/*   <Route path="/" element={<Home />} /> */}
+           <Route path="/EventForm" element={<EventForm />} />
           </Route>
         </Routes>
       </BrowserRouter>
       <EventForm />
     </div>
->>>>>>> jt
+
   );
 }
 
