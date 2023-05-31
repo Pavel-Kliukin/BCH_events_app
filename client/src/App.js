4b9c4d32f06f0177310987a6311079a980c0fefd
janibehm <jani.behm@gmail.com>
2023-05-31 23:47:14 +0300

    fetching data on cards on showing them on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EventForm from "./components/EventForm";
 import Layout from "./components/Layout";
-/* import Home from "./components/Home"; */
+import useFetch from './hooks/useFetch';
+import Home from "./components/Home";
 
 function App() {
-  return (
+  const { data, isLoading, error } = useFetch('http://localhost:3001/events');
 
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  // Now you can use the fetched data
+  console.log(data);
+
+  return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-        {/*   <Route path="/" element={<Home />} /> */}
-           <Route path="/EventForm" element={<EventForm />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <EventForm />
+      <Router>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/EventForm" element={<EventForm />} />
+          </Routes>
+       
+      </Router>
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,29 @@
+import React from 'react';
+import style from './Home.module.css';
+import Header from './Header';
+import Footer from './Footer';
+import Card from './Card';
+import useFetch from '../hooks/useFetch';
+
 const Home = () => {
-    return <h1>Welcome to the Homepage</h1>;
-}
+    const { data, isLoading, error } = useFetch('http://localhost:3001/events');
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div className={style.layout}>
+            <Header />
+            <div className="grid-container">
+                {data.map((eventData) => (
+                    <Card key={eventData.id} data={eventData} />
+                ))}
+            </div>
+            <Footer />
+
+        
+        </div>
+    );
+};
 
 export default Home;
