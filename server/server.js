71f367f2147311894e32979bf611a4a08efc3fb9
janibehm <jani.behm@gmail.com>
2023-06-07 20:06:01 +0300

    working on server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log("Successfully connected to MongoDB!");
+});
+
+const PostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', PostSchema);
+
+app.get('/posts', async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+
+app.post('/posts', async (req, res) => {
+  const newPost = new Post(req.body);
+  const savedPost = await newPost.save();
+  res.json(savedPost);
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const foundPost = await Post.findById(req.params.id);
+  res.json(foundPost);
+});
+
+app.patch('/posts/:id', async (req, res) => {
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {new: true});
+  res.json(updatedPost);
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const deletedPost = await Post.findByIdAndDelete(req.params.id);
+  res.json(deletedPost);
+});
+
+
 /* const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
