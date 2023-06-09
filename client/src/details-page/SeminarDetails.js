90b748627c087c1428305a8363f6bd2ab6c23612
Pavel-Kliukin <9213087@mail.ru>
2023-06-09 05:37:15 +0300

    All style done

--- client/src/details-page/SeminarDetails.js
+++ client/src/details-page/SeminarDetails.js
@@ -1,4 +1,8 @@
 import { useParams, Link } from "react-router-dom";
+import style from './SeminarDetails.module.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
 
 const SeminarDetails = ({ events, seminars }) => {
     const { seminarId } = useParams();
@@ -11,28 +15,53 @@ const SeminarDetails = ({ events, seminars }) => {
     const childEvents = events.filter((event) =>
     seminar.events_ids.includes(event.id)
   );
+
+  // This is for styling 'seminarImg' div
+  // The image will be set as a background of 'seminarImg div
+  const divStyle = {
+    backgroundImage: `url(${seminar.imgUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
   
     return (
         <div>
-        <h1>{seminar.seminarName}</h1>
-        <p>{seminar.Description}</p>
-        <p>Start Date: {seminar.startDay}</p>
-        <p>End Date: {seminar.endDay}</p>
-       
-        {childEvents && (
-        <div>
-          <p>This seminar hosts:</p>
-          <ul>
-            {childEvents.map((childEvent) => (
-              <li key={childEvent.id}>
-                <Link to={`/events/${childEvent.id}`}>
-                  {childEvent.eventName}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+          <Header />
+            <div className={style.seminarBody}>
+              <div className={style.seminarInfo}>
+                <div className={style.seminarImg} style={divStyle}></div>
+                <div className={style.titleAndDescription}>
+                  <h1>{seminar.seminarName}</h1>
+                  <div className={style.description}>
+                    {seminar.Description}
+                  </div>
+                  <div className={style.bottomBox}>
+                    <div className={style.dates}>
+                      <p><b>Start Day:</b> {seminar.startDay}</p>
+                      <p><b>End Day:</b> {seminar.endDay}</p>
+                    </div>
+                    <button>SUBSCRIBE</button>
+                  </div>
+                </div>
+              </div>
+              
+            
+              {childEvents && (
+                <div>
+                  <div className={style.hostBox}>
+                    <div className={style.hostText}>
+                      <h2>This seminar hosts:</h2>
+                    </div>
+                  </div>
+                  <div className={style.eventsContainer}>
+                    {childEvents.map((eventData) => (
+                        <Card key={eventData.id} data={eventData} seminars={seminars} />
+                    ))}
+                  </div> 
+                </div>
+              )}
+            </div>
+        <Footer />
       </div>
     );
   };
